36b45e7ca53b1ae774a9be8f25df84f9bb7a351b
cpeterson <cpeterson@collineargroup.com>
2019-06-26 09:48:10 -0700

    initial playground css grid created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="app">
+      <div className="grid-margin">grid</div>
+      <div className="grid-playground-container">
+        Test Area
+        <div>Stuff</div>
+      </div>
+      <div className="grid-margin">grid</div>
+    </div>
+  );
 }
 
 export default App;
